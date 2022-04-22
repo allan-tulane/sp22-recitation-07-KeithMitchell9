5142cfee59a174426480cd15d1b6348c5261432f
Keith <98364059+KeithMitchell9@users.noreply.github.com>
2022-04-22 05:06:35 +0000

    Completed

--- main.py
+++ main.py
@@ -2,57 +2,65 @@ import math, queue
 from collections import Counter
 
 class TreeNode(object):
-    # we assume data is a tuple (frequency, character)
-    def __init__(self, left=None, right=None, data=None):
-        self.left = left
-        self.right = right
-        self.data = data
-    def __lt__(self, other):
-        return(self.data < other.data)
-    def children(self):
-        return((self.left, self.right))
+  def __init__(self, left=None, right=None, data=None):
+    self.left = left
+    self.right = right
+    self.data = data
+  def __lt__(self, other):
+    return(self.data < other.data)
+  def children(self):
+    return((self.left, self.right))
+
     
 def get_frequencies(fname):
-    ## This function is done.
-    ## Given any file name, this function reads line by line to count the frequency per character. 
-    f=open(fname, 'r')
-    C = Counter()
-    for l in f.readlines():
-        C.update(Counter(l))
-    return(dict(C.most_common()))
+  f=open(fname, 'r')
+  C = Counter()
+  for l in f.readlines():
+    C.update(Counter(l))
+  return(dict(C.most_common()))
 
-# given a dictionary f mapping characters to frequencies, 
-# create a prefix code tree using Huffman's algorithm
+
 def make_huffman_tree(f):
-    p = queue.PriorityQueue()
-    # construct heap from frequencies, the initial items should be
-    # the leaves of the final tree
-    for c in f.keys():
-        p.put(TreeNode(None,None,(f[c], c)))
+  p = queue.PriorityQueue()
 
-    # greedily remove the two nodes x and y with lowest frequency,
-    # create a new node z with x and y as children,
-    # insert z into the priority queue (using an empty character "")
-    while (p.qsize() > 1):
-        # TODO
+  for c in f.keys():
+    p.put(TreeNode(None,None,(f[c], c)))
+
+  while (p.qsize() > 1):
+    x = p.get()
+    y = p.get()
+    z = x.data[0] + y.data[0]
+    p.put(TreeNode(x, y, (z, "")))
         
-    # return root of the tree
-    return p.get()
+  return p.get()
 
-# perform a traversal on the prefix code tree to collect all encodings
+
 def get_code(node, prefix="", code={}):
-    # TODO - perform a tree traversal and collect encodings for leaves in code
-    pass
+  if (node.left!= None):
+    get_code(node.left, prefix + '0', code)
 
-# given an alphabet and frequencies, compute the cost of a fixed length encoding
+  if (node.right !=None):
+    get_code(node.right, prefix + '1', code)
+
+  if (node.left==None and node.right ==None):
+    code[node.data[1]] = prefix
+  return code
+
+
 def fixed_length_cost(f):
-    # TODO
-    pass
+  n = len(f.keys())
+  cost = math.ceil(math.log2(n))
+  fixed = 0
+  for i in f.values():
+    fixed += cost * int(i)
+  return fixed
 
-# given a Huffman encoding and character frequencies, compute cost of a Huffman encoding
+
 def huffman_cost(C, f):
-    # TODO
-    pass
+  huff = 0
+  for i in f:
+    huff += len(C[i]) * f[i]
+  return huff
 
 f = get_frequencies('f1.txt')
 print("Fixed-length cost:  %d" % fixed_length_cost(f))
